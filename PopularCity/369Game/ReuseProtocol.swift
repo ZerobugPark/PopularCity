9b9c3cb37e3d2ac97e076b043009f465e28eee1a
young <zerobugpark@gmail.com>
2025-01-08 23:13:08 +0900

    재사용 프로토콜 추가

---
+++ PopularCity/369Game/ReuseProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  ReuseProtocol.swift
+//  PopularCity
+//
+//  Created by youngkyun park on 1/8/25.
+//
+
+import Foundation
+
+@objc
+protocol ObjectSetup {
+    func Setup()
+    @objc optional func labelConfig()
+    @objc optional func textFieldConfig()
+    @objc optional func imageViewConfig()
+    @objc optional func textViewConfig()
+    
+}
